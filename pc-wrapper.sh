e8735ae2fdefffa4f47e4da68fd3f4770cd7e02f
Eugene <x@null.page>
2022-12-26 17:12:20 +0100

    Update pc-wrapper.sh

--- pc-wrapper.sh
+++ pc-wrapper.sh
@@ -8,7 +8,7 @@ do
     package_cloud push --verbose --yes ${1}/${v} ${2}/*.rpm
 done | tee rpmout
 
-for v in ubuntu/bionic ubuntu/focal ubuntu/hirsute ubuntu/impish ubuntu/jammy debian/jessie debian/stretch debian/buster debian/bullseye debian/bookworm
+for v in ubuntu/bionic ubuntu/focal ubuntu/hirsute ubuntu/impish ubuntu/jammy ubuntu/kinetic debian/jessie debian/stretch debian/buster debian/bullseye debian/bookworm
 do
     echo Pushing to $1/$v
     package_cloud push --yes ${1}/${v} ${2}/*.deb
